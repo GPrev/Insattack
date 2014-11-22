5178d771c45c3d65584faae5692d3972884984f6
GPrev <gabriel.prevosto@insa-rennes.fr>
2014-11-22 11:48:30 +0100

    Board 2

---
+++ INSAttack/INSAttack/Board.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace INSAttack
+{
+    public class Board
+    {
+        private Dictionary<INSAttack.Coord, INSAttack.Tile> m_tileTable;
+
+        public Board()
+        {
+            m_tileTable = new Dictionary<Coord, Tile>();
+            m_unitTable = new Dictionary<Coord,List<Unit>>();
+        }
+
+        public Dictionary<Coord, Tile> TileTable
+        {
+            get { return m_tileTable; }
+            set { m_tileTable = value; }
+        }
+        private Dictionary<INSAttack.Coord, List<INSAttack.Unit>> m_unitTable;
+
+        public Dictionary<INSAttack.Coord, List<INSAttack.Unit>> UnitTable
+        {
+            get { return m_unitTable; }
+            set { m_unitTable = value; }
+        }
+
+        public bool addUnit(Coord c, Unit u)
+        {
+            if (!UnitTable.ContainsKey(c))
+                UnitTable.Add(c, new List<Unit>());
+            UnitTable[c].Add(u);
+            return true;
+        }
+
+        public bool removeUnit(Unit u)
+        {
+            Coord c = find(u);
+            if (!c.exists()) //could not find unit to move
+                return false;
+            //else
+            return removeUnit(u, c);
+        }
+        public bool removeUnit(Unit u, Coord c)
+        {
+            return UnitTable[c].Remove(u);
+        }
+
+        public bool moveUnit(Unit u, Coord dest)
+        {
+            if(removeUnit(u))
+            {
+                return addUnit(dest, u);
+            }
+            return false;
+        }
+
+        public Coord find(Unit u)
+        {
+            foreach(var l in UnitTable)
+            {
+                if (l.Value.Exists(x => x == u))
+                    return l.Key;
+            }
+            return Coord.nowhere;
+        }
+    }
+}
---
+++ INSAttackTests/INSAttackTests/BoardTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using INSAttack;
+
+namespace INSAttackTests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        Board m_board;
+        Player m_p1;
+
+        [TestInitialize]
+        public void init()
+        {
+            m_board = new Board();
+            m_p1 = new Player();
+        }
+
+        [TestCleanup]
+        public void cleanup()
+        {
+
+        }
+
+        [TestMethod]
+        public void MovementTest()
+        {
+            Unit u1 = new Unit(m_p1);
+            Unit u2 = new Unit(m_p1);
+
+            Assert.IsTrue(m_board.addUnit(new Coord(1, 1), u1));    //u1 -> (1,1)
+            Assert.AreEqual(m_board.find(u1), new Coord(1, 1));
+
+            Assert.IsTrue(m_board.addUnit(new Coord(2, 2), u2));    //u2 -> (2,2)
+
+            Assert.IsTrue(m_board.moveUnit(u1, new Coord(2, 2)));     //u1 -> (2,2)
+            Assert.AreEqual(m_board.find(u1), new Coord(2, 2));
+            Assert.AreEqual(m_board.find(u2), new Coord(2, 2));
+
+            Assert.IsTrue(m_board.moveUnit(u1, new Coord(3, 3)));     //u1 -> (3,3)
+            Assert.AreEqual(m_board.find(u1), new Coord(3, 3));
+            Assert.AreEqual(m_board.find(u2), new Coord(2, 2));
+
+            Assert.IsTrue(m_board.removeUnit(u1));                  //u1 -> X
+            Assert.IsFalse(m_board.find(u1).exists());
+        }
+    }
+}
